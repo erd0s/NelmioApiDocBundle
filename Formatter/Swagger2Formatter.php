edf076ffb4a6f9c1eb15a63974b076bcb1389015
Dirk <dirk@dirkdirk.com>
2016-08-12 16:54:58 +1000

    ADDED: A bunch of extra work on the model definitions

--- Formatter/Swagger2Formatter.php
+++ Formatter/Swagger2Formatter.php
@@ -9,14 +9,14 @@ class Swagger2Formatter implements FormatterInterface
     {
         $header = $this->getHeader();
         $paths = $this->getPaths($collection);
-        $result = array_merge($header, $paths);
+        $definitions = $this->getDefinitions($collection);
+        $result = array_merge($header, $paths, $definitions);
         return $result;
     }
 
     private function getHeader()
     {
         return [
-            'multilinestring' => "lkajsflkj asdlkfjasl dkfj asldkfj asdlkfj ads\n saldfkasdjflkasdjflk asdf\n\nasdfkjh",
             'swagger' => '2.0',
             'info' => [
                 'version' => '1.0.0',
@@ -66,15 +66,13 @@ class Swagger2Formatter implements FormatterInterface
                 $p["in"] = "path";
                 if (array_key_exists("dataType", $parameter)) $p["type"] = $parameter["dataType"];
                 if (array_key_exists("description", $parameter)) $p["description"] = $parameter["description"];
-                if (array_key_exists("required", $parameter)) $p["required"] = $parameter["required"];
+                if (array_key_exists("required", $parameter)) $p["required"] = $parameter["required"] == 'true' ? true : false;
 
                 $parameters[] = $p;
             }
             if (sizeof($parameters) > 0) $route[$method]["parameters"] = $parameters;
 
             // TODO the body parameters
-
-            $types = [];
 
             // Do the responses
             $responses = [];
@@ -83,26 +81,111 @@ class Swagger2Formatter implements FormatterInterface
 
                 // TODO schema
                 if (array_key_exists("class", $responseMap)) {
-                    $class = str_replace("\\", "/", $responseMap["class"]);
                     $r["schema"] = [
-                        "\$ref" => "#/definitions/$class"
+                        "\$ref" => "#/definitions/".$this->getSmallClass($responseMap["class"])
                     ];
-                    $types[$class] = "";
                 }
 
                 // TODO description
                 if (array_key_exists("description", $responseMap)) $r["description"] = $responseMap["description"];
+                else $r['description'] = '';
 
                 $responses[$statusCode] = $r;
             }
             if (sizeof($responses) > 0) $route[$method]["responses"] = $responses;
 
-
-            $result["paths"]["something"] = "lkjasflk jlaskdfjalsdfkj asd\n\nasldkfjalsdkfj";
             $result["paths"][$item["resource"]] = $route;
         }
-
         return $result;
+    }
+
+    private function getSmallClass($className) {
+        preg_match("/[a-zA-Z0-9]+$/", $className, $c);
+        if (sizeof($c) > 0) {
+            return $c[0];
+        }
+        else {
+            return $className;
+        }
+    }
+
+    private function getTypes($currentTypes, $fields) {
+        $walk = function($fields) use (&$currentTypes, &$walk) {
+            foreach ($fields as $field) {
+                if ($field["subType"] != null && !array_key_exists($field["subType"], $currentTypes)) {
+                    $currentTypes[$field["subType"]] = $field["children"];
+                    $walk($field["children"]);
+                }
+            }
+        };
+
+        $walk($fields);
+        return $currentTypes;
+    }
+
+    private function getDefinitions(array $collection)
+    {
+        $definitions = [];
+        foreach ($collection as $item) {
+            $annotation = $item['annotation'];
+
+            // Add response maps to potential type definitions
+            foreach ($annotation->getParsedResponseMap() as $response) {
+                $className = $this->getSmallClass($response['type']['class']);
+
+                if (!array_key_exists($className, $definitions)) {
+                    $definitions[$className] = $response["model"];
+                    $definitions = $this->getTypes($definitions, $response['model']);
+                }
+            }
+        }
+
+        // Turn the definitions into swagger format
+        $swaggerDef = [];
+
+        foreach ($definitions as $name => $definition) {
+            $type = ['type' => 'object'];
+
+            // Iterate over the properties
+            foreach ($definition as $fieldName => $fieldValue) {
+                $field = [];
+
+                if ($fieldValue['subType'] != null) {
+                    // This is either an object or a collection
+                    $className = $this->getSmallClass($fieldValue['subType']);
+                    if ($className == "AircraftGalleryImage") {
+                        $var = 1;
+                    }
+                    if ($fieldValue['actualType'] == "collection") {
+                        $field['type'] = "array";
+                        $field['items'] = ['$ref' => "#/definitions/$className"];
+                    }
+                    else {
+                        $field['$ref'] = "#/definitions/$className";
+                    }
+                }
+                else {
+                    // Just a normal scalar
+                    if ($fieldValue['dataType'] == "DateTime") {
+                        $field['type'] = "string";
+                        $field['format'] = "dateTime";
+                    }
+                    else {
+                        $field['type'] = $fieldValue['dataType'];
+                    }
+                }
+
+                if (array_key_exists("description", $fieldValue)) {
+                    if ($fieldValue['description'] != "") $field['description'] = $fieldValue['description'];
+                }
+
+                $type['properties'][$fieldName] = $field;
+            }
+
+            $swaggerDef['definitions'][$this->getSmallClass($name)] = str_replace("\\", "/", $type);
+        }
+
+        return $swaggerDef;
     }
 
     public function formatOne(ApiDoc $annotation)
